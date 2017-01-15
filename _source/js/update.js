7a5b58456a59a1f7260b168c3bf0c2545d452608
uralozden <uralozden@gmail.com>
2017-01-15 13:03:05 +0300

    Vue  data binding from service url

--- _source/js/update.js
+++ _source/js/update.js
@@ -1,8 +1,20 @@
 window.addEventListener('load',() => {
+
+    var ApiUrl =""; /* YOUR API URL */
     var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        query :"",
+        result: {}
+    },
+    methods: {
+        search:function(){
+            fetch(ApiUrl+this.query)
+            .then(result => result.json())
+            .then(result => {
+                this.$set(this,'result',result)
+            })
+        }
     }
     });
 })
