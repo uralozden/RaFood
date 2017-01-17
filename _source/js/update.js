f122469e666305a97ddcdefc6a0c87f4b31e4502
uralozden <uralozden@gmail.com>
2017-01-17 23:36:47 +0300

    added new design files

--- _source/js/update.js
+++ _source/js/update.js
@@ -1,20 +1,14 @@
 window.addEventListener('load',() => {
 
-    var ApiUrl =""; /* YOUR API URL */
+    var ApiUrl ="http://localhost/Rafood/api/api.php/food_info/"; /* YOUR API URL */
     var app = new Vue({
     el: '#app',
-    data: {
-        query :"",
-        result: {}
-    },
-    methods: {
-        search:function(){
-            fetch(ApiUrl+this.query)
-            .then(result => result.json())
-            .then(result => {
-                this.$set(this,'result',result)
-            })
-        }
+    ready:function(){
+        fetch(ApiUrl+"53")
+        .then(result => result.json())
+        .then(result => {
+            this.$set(this,'result',result)
+        })
     }
     });
 })
