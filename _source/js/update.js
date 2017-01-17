7b9e23c1ac79a3d3d6cc0d77d5a9921ca2395891
uralozden <uralozden@gmail.com>
2017-01-17 23:48:37 +0300

    GetFood function triggered

--- _source/js/update.js
+++ _source/js/update.js
@@ -3,12 +3,19 @@ window.addEventListener('load',() => {
     var ApiUrl ="http://localhost/Rafood/api/api.php/food_info/"; /* YOUR API URL */
     var app = new Vue({
     el: '#app',
-    ready:function(){
-        fetch(ApiUrl+"53")
-        .then(result => result.json())
-        .then(result => {
-            this.$set(this,'result',result)
-        })
+    data: {
+        query :"",
+        result: false
+    },
+    methods: {
+        GetFood:function(){
+            rndNumber = Math.floor((Math.random() * 400) + 20);
+            fetch(ApiUrl+rndNumber)
+            .then(result => result.json())
+            .then(result => {
+                this.$set(this,'result',result)
+            })
+        }
     }
     });
 })
